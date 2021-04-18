7d070002b3204927d13356c58e4e96046c90e3ee
Richard Neumann <mail@richard-neumann.de>
2021-04-18 17:08:16 +0200

    Remove unneeded template parameters

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,16 @@ using std::vector;
 	Capital values and rates.
 */
 void task1() {
-	vector<double> p1rates = {-35, 38.5, 127.05};
-	vector<double> p2rates = {-100, 165, 242};
-	vector<double> p3rates = {-50, 110, 60.5};
+	vector p1rates = {-35.0, 38.5, 127.05};
+	vector p2rates = {-100.0, 165.0, 242.0};
+	vector p3rates = {-50.0, 110.0, 60.5};
 	double interestRate = 0.1;
 
 	FinancingProject p1(p1rates, interestRate);
 	FinancingProject p2(p2rates, interestRate);
 	FinancingProject p3(p3rates, interestRate);
 
-	vector<FinancingProject> projects = {p1, p2, p3};
+	vector projects = {p1, p2, p3};
 	cout << "######### Projects ##########" << endl;
 
 	for (FinancingProject project : projects)
@@ -30,7 +30,7 @@ void task1() {
 	cout << "####### Investments #########" << endl;
 
 	double budget = 50;
-	vector<double> investments = investOptimal(budget, projects);
+	vector investments = investOptimal(budget, projects);
 
 	for (long unsigned int i = 0; i < investments.size(); i++)
 		cout << projects[i] << "Investment: " << investments[i] << endl << endl;
