3c6d8b244158f441038f36be165bc47023d53913
Richard Neumann <mail@richard-neumann.de>
2021-04-20 15:48:00 +0200

    Add task2

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 #include <vector>
 
 #include "task1.h"
+#include "task2.h"
 
 int main() {
 	task1();
+	task2();
 	return 0;
 }
---
+++ src/task2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+#include "task2.h"
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+void task2()
+{
+	vector p1rates = {-60.0, 120.0};
+	vector p2rates = {-90.0, 120.0};
+	vector p3rates = {-150.0, 150.0};
+	vector p4rates = {-100.0, 75.0};
+	double interestRate = 0.1;
+
+	FinancingProject p1(p1rates, interestRate);
+	FinancingProject p2(p2rates, interestRate);
+	FinancingProject p3(p3rates, interestRate);
+	FinancingProject p4(p4rates, interestRate);
+
+	vector projects = {p1, p2, p3, p4};
+	cout << "######### Projects ##########" << endl;
+
+	for (FinancingProject project : projects)
+		cout << project << endl;
+
+	cout << "####### Investments #########" << endl;
+
+	double budget = 400;
+	vector investments = investOptimal(budget, projects);
+
+	for (long unsigned int i = 0; i < investments.size(); i++)
+		cout << projects[i] << "Investment: " << investments[i] << endl << endl;
+}
