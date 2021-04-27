c0617bb2fe2427a0fb9891b821ab51a0a3403a24
Richard Neumann <mail@richard-neumann.de>
2021-04-27 11:56:03 +0200

    Refactoring

--- src/topic2.cpp
+++ src/topic2.cpp
@@ -15,17 +15,19 @@ using bufi::FRMInRears;
 */
 void topic2::example1()
 {
-	cout << "* Example PVA:" << endl;
 	double cashflow = 10000.0;		// ten thousand euros
 	double interestRate = 4.0 / 100;	// four percent
 	int runtime = 3; 			// three years
+
+	double pva = presentValue(interestRate, runtime);
+	double frmRears = FRMInRears(cashflow, interestRate, runtime);
+	double frmAdvance = FRMInAdvance(cashflow, interestRate, runtime);
+
+	cout << "* Example PVA:" << endl;
 	cout << "Interest rate: " << interestRate << endl;
 	cout << "Runtime: " << runtime << endl;
-	double pva = presentValue(interestRate, runtime);
 	cout << "=> PVA: " << pva << endl;
-	double frmRears = FRMInRears(cashflow, interestRate, runtime);
 	cout << "=> FRM in rears: " << frmRears << endl;
-	double frmAdvance = FRMInAdvance(cashflow, interestRate, runtime);
 	cout << "=> FRM in advance: " << frmAdvance << endl;
 }
 
