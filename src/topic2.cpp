67673465f9a9f4436afcccfb060d0c2d9bbd408b
Richard Neumann <mail@richard-neumann.de>
2021-04-27 11:54:56 +0200

    Add FRM examples

--- src/topic2.cpp
+++ src/topic2.cpp
@@ -7,6 +7,8 @@
 using std::cout;
 using std::endl;
 using bufi::presentValue;
+using bufi::FRMInAdvance;
+using bufi::FRMInRears;
 
 /*
 	Lecture example calculation.
@@ -14,12 +16,17 @@ using bufi::presentValue;
 void topic2::example1()
 {
 	cout << "* Example PVA:" << endl;
+	double cashflow = 10000.0;
 	double interestRate = 4.0 / 100;	// four percent
 	int runtime = 3; 			// three years
 	cout << "Interest rate: " << interestRate << endl;
 	cout << "Runtime: " << runtime << endl;
 	double pva = presentValue(interestRate, runtime);
 	cout << "=> PVA: " << pva << endl;
+	double frmRears = FRMInRears(cashflow, interestRate, runtime);
+	cout << "=> FRM in rears: " << frmRears << endl;
+	double frmAdvance = FRMInAdvance(cashflow, interestRate, runtime);
+	cout << "=> FRM in advance: " << frmAdvance << endl;
 }
 
 void topic2::run_tasks()
